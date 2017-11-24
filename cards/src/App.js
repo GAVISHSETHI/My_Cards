1e2ee7486ab99e76f84e99e4a7fe0131e23e28b9
gavishsethi <gavish.sethi@evivehealth.com>
2017-11-24 11:37:18 +0530

    deleted commented code

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,25 +1,3 @@
-// import React, { Component } from 'react';
-// import { Grid ,  } from 'semantic-ui-react';
-// import 'semantic-ui-css/semantic.min.css';
-// import './App.css';
-// import CardFull from './components/card-full';
-//
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//           <Grid centered columns={3}>
-//               <Grid.Column>
-//           <CardFull/>
-//               </Grid.Column>
-//           </Grid>
-//           </div>
-//     );
-//   }
-// }
-//
-// export default App;
 
 import React, { Component } from 'react';
 import { Grid , Button } from 'semantic-ui-react';
